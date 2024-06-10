b48d724d8ea5ed5b9c7d4f2b78a623e1a2a4ae13
Plinio <pliniopalmer@gmail.com>
2024-06-10 14:14:13 -0400

    Repositorio y clase tipo enum para la comprar

--- src/Entidades/DatoCompraCliente.java
+++ src/Entidades/DatoCompraCliente.java
@@ -9,6 +9,84 @@ public class DatoCompraCliente {
     private String TipoCompra;
     private String Artículo;
     private int    Cantidad;
-    private int    PrecioUnitario;
+    private double PrecioUnitario;
     private float  Monto;
+
+    public DatoCompraCliente(String Cedula, String Fecha, String Vendedor, String TipoCompra, String Artículo, int Cantidad, double PrecioUnitario, float Monto) {
+        this.Cedula = Cedula;
+        this.Fecha = Fecha;
+        this.Vendedor = Vendedor;
+        this.TipoCompra = TipoCompra;
+        this.Artículo = Artículo;
+        this.Cantidad = Cantidad;
+        this.PrecioUnitario = PrecioUnitario;
+        this.Monto = Monto;
+    }
+
+    
+    public String getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(String Cedula) {
+        this.Cedula = Cedula;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(String Fecha) {
+        this.Fecha = Fecha;
+    }
+
+    public String getVendedor() {
+        return Vendedor;
+    }
+
+    public void setVendedor(String Vendedor) {
+        this.Vendedor = Vendedor;
+    }
+
+    public String getTipoCompra() {
+        return TipoCompra;
+    }
+
+    public void setTipoCompra(String TipoCompra) {
+        this.TipoCompra = TipoCompra;
+    }
+
+    public String getArtículo() {
+        return Artículo;
+    }
+
+    public void setArtículo(String Artículo) {
+        this.Artículo = Artículo;
+    }
+
+    public int getCantidad() {
+        return Cantidad;
+    }
+
+    public void setCantidad(int Cantidad) {
+        this.Cantidad = Cantidad;
+    }
+
+    public double getPrecioUnitario() {
+        return PrecioUnitario;
+    }
+
+    public void setPrecioUnitario(int PrecioUnitario) {
+        this.PrecioUnitario = PrecioUnitario;
+    }
+
+    public float getMonto() {
+        return Monto;
+    }
+
+    public void setMonto(float Monto) {
+        this.Monto = Monto;
+    }
+    
+    
 }
--- src/proyectoexoneraconii/ProyectoExoneraconII.java
+++ src/proyectoexoneraconii/ProyectoExoneraconII.java
@@ -5,6 +5,7 @@
  */
 package proyectoexoneraconii;
 
+import Entidades.TipoCompra;
 import Entidades.Vendedor;
 import Repositorios.CVendedorRepositorio;
 import java.util.ArrayList;
@@ -19,26 +20,7 @@ public class ProyectoExoneraconII {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        CVendedorRepositorio repo = new CVendedorRepositorio();
-        
-        //CREAR VENDEDOR
-        //String cedula, String nombre, String genero, boolean estado, String rol, String pass
-        //Vendedor ven = new Vendedor("00000000000","Maxwell M","Masculino",true,"ADMIN","123456");
-        
-        System.out.println(repo.Eliminar("00000000000")); 
-        
-        //System.out.println(repo.Crear(ven));
-        //Vendedor persona = repo.Get("40238830059");
-        //System.out.println(persona.getNombre());
-        
-        //ArrayList<Vendedor> lista = repo.Get(); 
-        
-        //for (Vendedor persona : lista) {
-         //   System.out.println(persona.getCedula());
-       // }
- 
+        System.out.println(TipoCompra.Cheque);
     }
     
 }
