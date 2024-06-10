c13702f51b57eb00de2737e8ea2ce521df58b90e
Plinio <pliniopalmer@gmail.com>
2024-06-10 14:14:44 -0400

    Repositorio y clase tipo enum para la comprar

---
+++ src/Repositorios/CDatoCompraClienteRepositorio.java
@@ -0,0 +1,91 @@
+package Repositorios;
+
+import Entidades.DatoCompraCliente;
+import Interfaces.ICRUDDB;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class CDatoCompraClienteRepositorio implements ICRUDDB<DatoCompraCliente> {
+
+    CConexion _conexion = null;
+
+    public CDatoCompraClienteRepositorio() {
+        _conexion = new CConexion();
+    }
+
+    @Override
+    public int Crear(DatoCompraCliente entidad) {
+        String Sql = "INSERT INTO [dbo].[DatoCompraCliente] ([Cedula] ,[Fecha] ,[Vendedor] ,[TipoCompra] ,[Articulo] ,[Cantidad] ,[PrecioUnitario] ,[Monto]) VALUES (? ,?> ,?> ,?> ,?> ,> ,?> ,?)";
+
+        try {
+            CallableStatement cs = _conexion.establecerConexion().prepareCall(Sql);
+
+            cs.setString(1, entidad.getCedula());
+            cs.setString(2, entidad.getFecha());
+            cs.setString(3, entidad.getVendedor());
+            cs.setString(4, entidad.getTipoCompra());
+            cs.setString(5, entidad.getArtículo());
+            cs.setInt(6, entidad.getCantidad());
+            cs.setDouble(7, entidad.getPrecioUnitario());
+            cs.setDouble(8, entidad.getMonto());
+
+            cs.execute();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error a insertar: " + e.getMessage());
+            return 0;
+        }
+        return 1;
+
+    }
+
+    @Override
+    public DatoCompraCliente Get(String x) {
+        DatoCompraCliente datoCompracliente = null;
+        String Sql = "SELECT * FROM DatoCompraCliente WHERE Cedula = '" + x + "'";
+        Statement st;
+
+        try {
+            st = _conexion.establecerConexion().createStatement();
+
+            ResultSet rs = st.executeQuery(Sql);
+
+            while (rs.next()) {
+//     
+                datoCompracliente = new DatoCompraCliente(rs.getString("Cedula"),
+                        rs.getString("Fecha"),
+                        rs.getString("Vendedor"),
+                        rs.getString("TipoCompra"),
+                        rs.getString("Articulo"),
+                        rs.getInt("Cantidad"),
+                        rs.getDouble("PrecioUnitario"),
+                        rs.getFloat("Monto")
+                );
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al obtener la lista: " + e.getMessage());
+        }
+
+        return datoCompracliente;
+    }
+
+    @Override
+    public ArrayList<DatoCompraCliente> Get() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int Eliminar(String x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int Modificar(DatoCompraCliente entidad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
